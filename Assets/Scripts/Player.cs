c0ab8e501c94ea61217accfddd82a45e1e2a679f
oliveirajne <oliveirajne@gmail.com>
2023-12-12 02:10:17 -0300

    player horizontal movement (wip)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,20 +9,33 @@ public class Player : MonoBehaviour
 
     private float speed = 10.0f;
 
+    private Rigidbody2D rb;
+
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
     }
 
 
     void Update()
     {
-    	float moveX = Input.GetAxisRaw("Horizontal");
+    	float moveX = Input.GetAxisRaw(PlayerAnimationParameters.axisXinput);
 
-        animator.SetFloat("moveX", moveX);
+        animator.SetFloat(PlayerAnimationParameters.moveX, moveX);
 
         bool isMoving = !Mathf.Approximately(moveX, 0f);
 
-        animator.SetBool("isMoving", isMoving);
+        animator.SetBool(PlayerAnimationParameters.isMoving, isMoving);
+    }
+
+    void FixedUpdate()
+    {
+        float forceX = animator.GetFloat(PlayerAnimationParameters.forceX);
+
+        if (forceX != 0) rb.AddForce(new Vector2(forceX, 0) * Time.deltaTime);
+
+        float impulseY = animator.GetFloat(PlayerAnimationParameters.impulseY);
+
+        if (impulseY != 0) rb.AddForce(new Vector2(0, impulseY), ForceMode2D.Impulse);
     }
 }
---
+++ Assets/Scripts/PlayerAnimationParameters.cs
@@ -0,0 +1,8 @@
+public static class PlayerAnimationParameters {
+    public const string moveX = "moveX";
+    public const string isMoving = "isMoving";
+    public const string axisXinput = "Horizontal";
+    public const string forceX = "forceX";
+    public const string impulseY = "impulseY";
+
+}
